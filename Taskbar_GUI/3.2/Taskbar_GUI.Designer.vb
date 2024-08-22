1ed716f5b44601fae85c7d245f737a864aba7875
stopyouhelper24 <101508503+stopyouhelper24@users.noreply.github.com>
2024-08-22 21:02:15 +0200

    Add files via upload

---
+++ Taskbar_GUI/3.2/Taskbar_GUI.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Taskbar_GUI
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.BackgroundImage = Global.CV.My.Resources.Resources._3643769_building_home_house_main_menu_start_1134161
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(572, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(76, 69)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.BackgroundImage = Global.CV.My.Resources.Resources.Search_find_locate_1542
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(654, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(70, 69)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = False
+        Me.Button2.Visible = False
+        '
+        'Taskbar_GUI
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Taskbar_GUI"
+        Me.Size = New System.Drawing.Size(1219, 69)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Taskbar_GUI/3.2/Taskbar_GUI.vb
@@ -0,0 +1,44 @@
+﻿Public Class Taskbar_GUI
+    Private Menu_Mode_Looker As String = "StartMenu"
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Form_1pad.Menu_ = "0" Then
+            If Form_1pad.StartMenuPanel2.Visible = True Then
+                Form_1pad.StartMenuPanel2.Visible = False
+            ElseIf Form_1pad.StartMenuPanel2.Visible = False Then
+                'Contains Search Menu Shit
+                If UI.Search_Menu = True Then
+                    Menu_Mode_Looker = "Search"
+                    Form_1pad.StartMenuPanel2.Controls.Remove(UI.StartMenuGUI)
+                    Form_1pad.StartMenuPanel2.Controls.Remove(Form_1pad.StartMenuControls)
+                    UI.SearchMenuGUI.Dock = DockStyle.Fill
+                    Form_1pad.StartMenuPanel2.Controls.Add(UI.SearchMenuGUI)
+                ElseIf Form_1pad.StartMenuPanel2.Controls.Contains(UI.SearchMenuGUI) Then
+                    Menu_Mode_Looker = "StartMenu"
+                    Form_1pad.StartMenuPanel2.Controls.Remove(UI.SearchMenuGUI)
+                    Form_1pad.StartMenuPanel2.Controls.Remove(Form_1pad.StartMenuControls)
+                    Form_1pad.StartMenuPanel2.Controls.Add(UI.StartMenuGUI)
+                End If
+                Form_1pad.StartMenuPanel.Visible = True
+                Form_1pad.StartMenuPanel2.Visible = True
+                Form_1pad.StartMenuPanel2.BackColor = Form_1pad.TaskbarPanel.BackColor
+                Form_1pad.StartMenuControls.Panel1.Visible = True
+                UI.StartMenuGUI.Label1.Text = Form_1pad.Username
+            End If
+
+        ElseIf Form_1pad.Menu_ = "1" Then
+            Form30.LoadUI("{B5A2FE71-4025-4025-9824-5D7D57803690}")
+        End If
+
+    End Sub
+
+    Private Sub Taskbar_GUI_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        Form_1pad.TaskbarPanel_MouseDown(Me, e)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        UI.Search_Menu = True
+        Button1_Click(Me, e)
+        UI.Search_Menu = False
+    End Sub
+End Class
