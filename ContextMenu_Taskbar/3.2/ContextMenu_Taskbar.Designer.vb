afa3d282e86496cf7a3646736a31feafe15051e0
sebastian2007bro <70060074+sebastian2007bro@users.noreply.github.com>
2025-01-16 13:36:45 +0100

    Add files via upload

---
+++ ContextMenu_Taskbar/3.2/ContextMenu_Taskbar.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ContextMenu_Taskbar
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.RunToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TaskbarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.Transparent
+        Me.MenuStrip1.Font = New System.Drawing.Font("Trebuchet MS", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RunToolStripMenuItem, Me.TaskbarToolStripMenuItem})
+        Me.MenuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.VerticalStackWithOverflow
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(174, 62)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'RunToolStripMenuItem
+        '
+        Me.RunToolStripMenuItem.ForeColor = System.Drawing.Color.White
+        Me.RunToolStripMenuItem.Name = "RunToolStripMenuItem"
+        Me.RunToolStripMenuItem.Size = New System.Drawing.Size(167, 28)
+        Me.RunToolStripMenuItem.Text = "Run"
+        '
+        'TaskbarToolStripMenuItem
+        '
+        Me.TaskbarToolStripMenuItem.ForeColor = System.Drawing.Color.White
+        Me.TaskbarToolStripMenuItem.Name = "TaskbarToolStripMenuItem"
+        Me.TaskbarToolStripMenuItem.Size = New System.Drawing.Size(167, 28)
+        Me.TaskbarToolStripMenuItem.Text = "Taskbar Settings"
+        '
+        'ContextMenu_Taskbar
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Transparent
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "ContextMenu_Taskbar"
+        Me.Size = New System.Drawing.Size(174, 65)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents RunToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TaskbarToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ ContextMenu_Taskbar/3.2/ContextMenu_Taskbar.vb
@@ -0,0 +1,19 @@
+﻿Public Class ContextMenu_Taskbar
+    Private Sub RunToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RunToolStripMenuItem.Click
+        If UI.LockedMode = True Then
+        Else
+            UI.StartRun()
+        End If
+
+        Form_1pad.Panel2.Visible = False
+        Form_1pad.Panel4.Visible = False
+    End Sub
+
+    Private Sub TaskbarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TaskbarToolStripMenuItem.Click
+        Form44.Show()
+        Form44.TextBox1.Text = "settings/taskbar-settings"
+        Form44.SEV()
+        Form_1pad.Panel2.Visible = False
+        Form_1pad.Panel4.Visible = False
+    End Sub
+End Class
