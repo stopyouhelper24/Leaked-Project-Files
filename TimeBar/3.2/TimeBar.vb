f83e599d008fac7199a905ea4dc1249a76fd8c7b
sebastian2007bro <70060074+sebastian2007bro@users.noreply.github.com>
2025-01-16 13:34:52 +0100

    Add files via upload

---
+++ TimeBar/3.2/TimeBar.vb
@@ -0,0 +1,14 @@
+﻿Public Class TimeBar
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label2.Text = Format(Now, "HH:mm:ss")
+        Label1.Text = Format(Now, "dd-MM-yyyy")
+    End Sub
+
+    Private Sub TimeBar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TimeBar_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        Form_1pad.TopPanel_MouseDown(Me, e)
+    End Sub
+End Class
